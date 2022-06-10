b42813d7f2acd238ef5947b80478240827c5b956
Rohann18 <rohann.bella@hotmail.fr>
2022-06-10 10:17:28 +0200

    Création du component Client avec affichage des clients

--- src/app/model/client.model.ts
+++ src/app/model/client.model.ts
@@ -1,2 +1,8 @@
 export class Client {
+  id!:number
+  nom!:string
+  prenom!:string
+  dateNaissance!:Date
+  mail!:string
+  adresse!:string
 }
--- src/app/service/client.service.ts
+++ src/app/service/client.service.ts
@@ -1,9 +1,18 @@
+import { environment } from './../../environments/environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Client } from '../model/client.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getAllClient():Observable<Client[]>
+  {
+    return this.httpClient.get<Client[]>(environment.url + "clients")
+  }
 }
