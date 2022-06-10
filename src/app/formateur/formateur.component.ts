c94ec1576214b60a80d7c65b8c9c7f8f70e51b0c
Benjosa <sablier.jo@hotmail.fr>
2022-06-10 11:09:42 +0200

    premier depuis eclipse

---
+++ src/app/formateur/formateur.component.ts
@@ -0,0 +1,23 @@
+import { FormateurService } from './../service/formateur.service';
+import { Formateur } from './../model/formateur.model';
+import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-formateur',
+  templateUrl: './formateur.component.html',
+  styleUrls: ['./formateur.component.css']
+})
+export class FormateurComponent implements OnInit {
+
+  listeFormateur$!: Observable<Formateur[]>
+
+  constructor(private router: Router, private formateurService: FormateurService) { }
+
+  ngOnInit(): void
+  {
+    this.listeFormateur$ = this.formateurService.getAllFormateur();
+  }
+
+}
