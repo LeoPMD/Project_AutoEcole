e39c2d76d72ab743b2c8728e8e65041a525315aa
Prasiinus <valentind9@hotmail.fr>
2022-06-15 14:43:08 +0200

    Commit before merge

--- src/app/formulaire-seance/formulaire-seance.component.ts
+++ src/app/formulaire-seance/formulaire-seance.component.ts
@@ -3,7 +3,9 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { SeanceConduite } from '../model/seance-conduite.model';
+import { Vehicule } from '../model/vehicule.model';
 import { SeanceConduiteService } from '../service/seance-conduite.service';
+import { VehiculeService } from '../service/vehicule.service';
 
 @Component({
   selector: 'app-formulaire-seance',
@@ -14,18 +16,24 @@ export class FormulaireSeanceComponent implements OnInit {
 
   seanceForm!: FormGroup
   listeSeances$!: Observable<SeanceConduite[]>
+  listeVehicules$!: Observable<Vehicule[]>
 
-  constructor(private formBuilder: FormBuilder, private router: Router, private seanceConduiteService: SeanceConduiteService) { }
+  constructor(private formBuilder: FormBuilder, private router: Router, private seanceConduiteService: SeanceConduiteService, private vehiculeService: VehiculeService) { }
 
   ngOnInit(): void {
+    this.listeSeances$=this.seanceConduiteService.getAllSeances()
+    this.listeVehicules$=this.vehiculeService.getAllVehicules()
     this.listeSeances$=this.seanceConduiteService.getAllSeances()
     this.seanceForm=this.formBuilder.group({
       dateSeance:[null, [Validators.required]],
       heureDebutSeance:[null, [Validators.required]],
       heureFinSeance:[null, [Validators.required]],
-      // vehicule:[null, [Validators.required]],
-      // formateur:[null, [Validators.required]],
+      vehicule:[null, [Validators.required]],
+      formateur:[null, [Validators.required]],
       })
+
+
+  
   }
 
 
