0319975844e8e05482f80d3da63f9470ffe11211
LeoPMD <leo.pellegrino2b@gmail.com>
2022-06-16 13:10:04 +0200

    Quiz et vidéo fonctionnels sur la page /quizCode

--- src/app/quiz-code/quiz-code.component.ts
+++ src/app/quiz-code/quiz-code.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 // declare function loadQuiz(): void;
-declare const loadQuiz: any;
-declare const deselectAnswers: any;
-declare const getSelected: any;
-declare const addEventListener: any;
+
+// declare const loadQuiz: any;
+// declare const deselectAnswers: any;
+// declare const getSelected: any;
+// declare const addEventListener: any;
+declare const initData: any;
 
 @Component({
   selector: 'app-quiz-code',
@@ -20,10 +22,11 @@ export class QuizCodeComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    loadQuiz();
-    deselectAnswers();
-    getSelected();
-    addEventListener();
+    initData();
+    // loadQuiz();
+    // deselectAnswers();
+    // getSelected();
+    // addEventListener();
   }
 
 }
--- src/assets/quizcode.js
+++ src/assets/quizcode.js
@@ -1,102 +1,111 @@
-const quizData = [
-    {
-        question: "Ceci est ma Première question",
-        a:"Réponse a",
-        b:"Réponse b",
-        c:"Réponse c",
-        d:"Réponse d",
-        correct: "c",
-    },
-    {
-        question: "Deuxième question",
-        a:"Réponse a",
-        b:"Réponse b",
-        c:"Réponse c",
-        d:"Réponse d",
-        correct: "c", 
-    },
-    {
-        question: "Troisème question",
-        a:"Réponse a",
-        b:"Réponse b",
-        c:"Réponse c",
-        d:"Réponse d",
-        correct: "c", 
-    },
-    {
-        question: "Quatrième question",
-        a:"Réponse a",
-        b:"Réponse b",
-        c:"Réponse c",
-        d:"Réponse d",
-        correct: "c", 
+function initData() {
+    
+    const quizData = [
+        {
+            question: "Quand je ne suis pas prioritaire sur une intersection je dois regarder si possible en premier les véhicules arrivant de :",
+            a: "La droite.",
+            b: "La gauche.",
+            c: "",
+            d: "",
+            correct: "b",
+        },
+        {
+            question: "La ligne transversale pour un stop est :",
+            a: "Continue.",
+            b: "En pointillée.",
+            c: "",
+            d: "",
+            correct: "a",
+        },
+        {
+            question: "Si à un feu rouge il y a la présence d'une flèche jaune clignotante à droite :",
+            a: "Je dois m'arrêter au feu rouge même si je tourne à droite.",
+            b: "Je peux m'engager à droite sans céder le passage.",
+            c: "Je peux m'engager à droite en cédant le passage aux piétons et véhicules circulant sur la voie abordée.",
+            d: "",
+            correct: "c",
+        },
+        {
+            question: "Si il n'y a aucune signalisation au niveau d'une intersection:",
+            a: "Je dois la priorité à droite.",
+            b: "Je serai prioritaire.",
+            c: "Je dois la priorité à droite et à gauche.",
+            d: "",
+            correct: "a",
+        },
+    
+    
+    ];
+    
+    const quiz= document.getElementById('quiz')
+
+    const answerEls = document.querySelectorAll('.answer')
+    const questionEl = document.getElementById('question')
+
+    const a_text = document.getElementById('a_text')
+    const b_text = document.getElementById('b_text')
+    const c_text = document.getElementById('c_text')
+    const d_text = document.getElementById('d_text')
+
+    const submitBtn = document.getElementById('submit')
+    
+    
+    let currentQuiz = 0
+    let score = 0
+    
+    loadQuiz()
+    
+    function loadQuiz() {
+    
+        deselectAnswers()
+    
+        const currentQuizData = quizData[currentQuiz]
+    
+        questionEl.innerText = currentQuizData.question
+        a_text.innerText = currentQuizData.a
+        b_text.innerText = currentQuizData.b
+        c_text.innerText = currentQuizData.c
+        d_text.innerText = currentQuizData.d
     }
- 
-];
-
-const quiz=document.getElementById('quiz')
-const reponseEls = document.querySelectorAll('.reponse') // "Els" signifie "list"
-const questionEl=document.getElementById('question')
-
-const a_text=document.getElementById('a_text')
-const b_text=document.getElementById('b_text')
-const c_text=document.getElementById('c_text')
-const d_text=document.getElementById('d_text')
-
-const submitBtn = document.getElementById('submit')
-
-
-let currentQuiz = 0
-let score = 0
-
-loadQuiz()
-
-function loadQuiz(){
-
-    deselectAnswers()
-
-    const currentQuizData = quizData[currentQuiz]
-
-    questionEl.innerText = currentQuizData.question
-    a_text.innerText = currentQuizData.a
-    b_text.innerText = currentQuizData.b
-    c_text.innerText = currentQuizData.c
-    d_text.innerText = currentQuizData.d
-}
-
-function deselectAnswers() {
-    reponseEls.forEach(reponseEl => reponseEl.checked = false)
-}
-
-function getSelected() {
-    let reponse
-    reponseEls.forEach(reponseEl => {
-        if(reponseEl.checked) {
-            reponse = reponseEl.id
+    
+    function deselectAnswers() {
+        answerEls.forEach(answerEl => answerEl.checked = false)
+    }
+    
+    function getSelected() {
+        let answer
+        answerEls.forEach(answerEl => {
+            if(answerEl.checked) {
+                answer = answerEl.id
+            }
+        })
+        return answer
+    }
+    
+    
+    submitBtn.addEventListener('click', () => {
+        const answer = getSelected()
+        if(answer) {
+           if(answer === quizData[currentQuiz].correct) {
+               score++
+           }
+    
+           currentQuiz++
+    
+           if(currentQuiz < quizData.length) {
+               loadQuiz()
+           } else {
+               quiz.innerHTML = `
+               <h2 style="padding: 20px">You answered ${score}/${quizData.length} questions correctly</h2>
+    
+               <button onclick="location.reload()">Reload</button>
+               `
+           }
         }
     })
-    return reponse
+
+
 }
 
-submitBtn.addEventListener('click', () => {
-    const reponse = getSelected()
-    if(reponse) {
-        if(reponse === quizData[currentQuiz].correct) {
-            score++
-        }
 
-        currentQuiz++
 
-        if(currentQuiz < quizData.length) {
-            loadQuiz() 
-        } else {
-            quiz.innerHTML = `
-			<h2>Tu as répondu a ${score}/${quizData.lenght} questions correctement</h2>
-            
-            <button onclick="location.reload()">Reload</button>
-			`
-        }
-    }
-}
-
-)
